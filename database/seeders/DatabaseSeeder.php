4b2b735c2b4fd1fd28f0cc50cb78a5b805058e0c
AiDinaAgustin <aidinaagustin2@gmail.com>
2025-09-08 13:17:30 +0700

    Add seed data for Sparepart model in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sparepart;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +15,23 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolePermissionSeeder::class);
+
+        Sparepart::create([
+            "name_sparepart" => "Oli Mesin",
+            "minimal_stok" => 5,
+            "stok" => 20
+        ]);
+
+        Sparepart::create([
+            "name_sparepart" => "Filter Oli",
+            "minimal_stok" => 5,
+            "stok" => 20
+        ]);
+
+        Sparepart::create([
+            "name_sparepart" => "Kampas Rem",
+            "minimal_stok" => 5,
+            "stok" => 20
+        ]);
     }
 }
